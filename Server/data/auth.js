a6b1db89dca1fcba38a67fc43dc4d7100ecc3fb9
leeCodingStudio <lehj2308@gmail.com>
2023-05-16 14:50:53 +0900

    mongoose 적용

--- Server/data/auth.js
+++ Server/data/auth.js
@@ -1,30 +1,25 @@
-import { getUsers } from '../db/database.js';
-import MongoDb from 'mongodb';
-const ObjectID = MongoDb.ObjectId;
+import Mongoose from "mongoose";
+import { useVirtualId } from '../db/database.js';
 
+const userSchema = new Mongoose.Schema({
+    username: { type:String, required: true },
+    name: { type:String, required: true },
+    email: { type:String, required: true },
+    password: { type:String, required: true },
+    url: String
+});
+
+useVirtualId(userSchema);
+const User = Mongoose.model('User', userSchema);
 
 export async function findByUsername(username){
-    return getUsers()
-    .find({ username })
-    .next()
-    .then(mapOptionalUser);
+    return User.findOne({ username });
 }
 
 export async function createUser(user){
-    return getUsers().insertOne(user)
-    .then((result) => {
-        console.log(result);
-        // result.ops[0]._id.toString();
-    })
+    return new User(user).save().then((data) => data.id);
 }
 
 export async function findById(id){
-    return getUsers()
-    .find({ _id: new ObjectID(id) })
-    .next()
-    .then(mapOptionalUser);
-}
-
-function mapOptionalUser(user){
-    return user ? { ...user, id: user._id.toString() } : user;
+    return User.findById(id);
 }
